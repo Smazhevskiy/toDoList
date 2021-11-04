19c2c2e6252054c245056ddfa76438dd07b4789f
user_nitro_5 <timmy.lepel@gmail.com>
2021-11-04 11:27:07 +0300

    hashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import reportWebVitals from './reportWebVitals';
-import App from './app/App';
-import {Provider} from 'react-redux';
-import {store} from './app/store';
-import {BrowserRouter} from "react-router-dom";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import reportWebVitals from './reportWebVitals'
+import App from './app/App'
+import {Provider} from 'react-redux'
+import {store} from './app/store'
+import {HashRouter} from 'react-router-dom'
 
 ReactDOM.render(
     <React.StrictMode>
-        <BrowserRouter>
+        <HashRouter>
             <Provider store={store}>
                 <App/>
             </Provider>
-        </BrowserRouter>
+        </HashRouter>
     </React.StrictMode>,
     document.getElementById('root')
 );
